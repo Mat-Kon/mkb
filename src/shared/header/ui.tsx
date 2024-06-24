184de4c1fe51464e39db6ca698b2e30ab1499817
Mat-Kon <mat_kon@mail.ru>
2024-06-24 22:32:39 +0300

    feat: add logo icon

--- src/shared/header/ui.tsx
+++ src/shared/header/ui.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import { Logo } from "../assets";
+import st from './styles.module.scss';
+
 export const Header = () => {
   return (
-    <header>header</header>
+    <header>
+      <Link to={'/'}>
+        <div className={st.iconWrapper}>
+        <Logo />
+        </div>
+      </Link>
+
+      <nav></nav>
+      <button></button>
+    </header>
   );
 };
