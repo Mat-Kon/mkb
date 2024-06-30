aa7a721dcad1b000521764df01939917b35e1280
Mat-Kon <mat_kon@mail.ru>
2024-06-30 22:23:22 +0300

    refactor: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@shared': path.resolve(__dirname, 'src', 'shared'),
+      '@src': path.resolve(__dirname, 'src'),
+      '@styles': path.resolve(__dirname, 'src', 'shared', 'styles'),
+      '@pages': path.resolve(__dirname, 'src', 'pages'),
+      '@entities': path.resolve(__dirname, 'src', 'entities'),
+      '@features': path.resolve(__dirname, 'src', 'features'),
+      '@widgets': path.resolve(__dirname, 'src', 'widgets'),
     }
   }
 })
